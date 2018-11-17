c39d6dbe290212f1b6367e4f76b2650466515e9f
Abose Ukhun <bexis17@gmail.com>
2018-11-17 15:12:01 +0000

    build route for getting a user's exercise log

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //connect to the database
 var mlabUrl = process.env.MONGOLAB_URI; /*get mlab url(credentials) from environment variables and url-shortner is dbname*/
 mongoose.Promise = global.Promise;
-mongoose.connect(mlabUrl ||"mongodb://localhost:27017/urlShortener"); /*connect to mongodb(on mlab, no locally) with mongoose
+mongoose.connect(mlabUrl ||"mongodb://localhost:27017/exerciseTracker"); /*connect to mongodb(on mlab, no locally) with mongoose
 either on mlab cloud remote database or locally installed mongodb*/
 
 var connection = mongoose.connection;
@@ -30,12 +30,10 @@ app.post("/api/exercise/new-user", function(request, response){
     //get username from user form
     var userName = request.body.username;
     
-    //create a userId
-    var userId = Math.floor(Math.random()*10000);
+    
     
     //create new user model
     var userInstance = new userModel({
-        id : userId,
         username : userName,
         count : 0,
         log : []
@@ -48,10 +46,10 @@ app.post("/api/exercise/new-user", function(request, response){
             response.send("Error saving to the database");
         }
         if(data){
-            //return back to the user only the original url and shortened form
+            //show user data
             response.send({
             userName : userName,
-            id : userId
+            id : data._id
             });
         }
     })
@@ -66,7 +64,7 @@ app.post("/api/exercise/add", function(request, response){
         "date" : request.body.date
     }
     //find document with this userId in the database collection usermodel
-    userModel.findOneAndUpdate({'id':userId},
+    userModel.findOneAndUpdate({'_id':userId},
     {$inc:{count: 1}, $push:{log: newLog }}, {new: true}, function(err, docs){
         if(err){
             response.send("cannot find this user Id in the database. Please"
@@ -74,18 +72,41 @@ app.post("/api/exercise/add", function(request, response){
         }
         if(docs){
             
-            //send user his details
+            //send user details back
             response.send({ 
                 "username" : docs.username,
                 "description" :request.body.description,
                 "duration" : request.body.duration,
-                "id": userId,
+                "id": docs._id,
                 "date": request.body.date
             });
         }
     })
 })
 
+app.get("/api/exercise/log?", function(request, response){
+    //get user id
+    var userId = request.query.userId;
+    //find document with this user id in the database collection userModel
+    userModel.findOne({_id : userId}, function(err, docs){
+        if(err){
+            response.send("cannot find your userId in database.");
+        }
+        else if(docs){
+          
+            response.send({
+                "id" : docs._id,
+                "username" : docs.username,
+                "count" : docs.count,
+                "log" : docs.log
+            });
+        }
+        else{
+            response.send({"error": "cannot find your userId in database."
+                + " Please create a new user"});
+        }
+    });
+});
 
 app.listen(port, function(){
     console.log("app is listening on port");
